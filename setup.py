7883f6feec9c6dd7d75f5a33279ad3096ab39be7
Johanna Engelhard <johanna.engelhard@hpe.com>
2022-02-18 09:00:19 +0200

    consistent versions

--- setup.py
+++ setup.py
@@ -9,8 +9,6 @@ setup(
     classifiers=[
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3",
     ],
