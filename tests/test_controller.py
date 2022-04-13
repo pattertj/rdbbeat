ff2fd003e3c6a828d643c6a60b962a5f86827e93
evanstjabadi <evansltjabadi@gmail.com>
2022-04-13 17:05:08 +0200

    use new fixtures

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -33,31 +33,16 @@ def test_schedule_task(scheduled_task):
         assert actual_scheduled_task.schedule == expected_scheduled_task.schedule
 
 
-def test_update_task_enable_status():
+def test_update_task_enable_status(scheduled_task):
     with patch("sqlalchemy.orm.Session") as mock_session:
-        schedule: Dict = {
-            "minute": "23",
-            "hour": "00",
-            "day_of_week": "2",
-            "day_of_month": "23",
-            "month_of_year": "12",
-            "timezone": "UTC",
-        }
-        scheduled_task: Dict = {
-            "name": "task_1",
-            "task": "echo",
-            "schedule": schedule,
-        }
-        schedule = CrontabSchedule(**schedule)
 
         mock_session.query(PeriodicTask).get.return_value = PeriodicTask(
             enabled=True,
-            crontab=schedule,
+            crontab=CrontabSchedule(**scheduled_task["schedule"]),
             name=scheduled_task["name"],
             task=scheduled_task["task"],
         )
         periodic_task_id = 1
-
         updated_task = update_task_enable_status(mock_session, False, periodic_task_id)
 
         assert updated_task.enabled is False
@@ -72,21 +57,9 @@ def test_update_task_enabled_status_fail():
             update_task_enable_status(mock_session, False, periodic_task_id)
 
 
-def test_update_periodic_task():
+def test_update_periodic_task(scheduled_task):
     with patch("sqlalchemy.orm.Session") as mock_session:
-        schedule: Dict = {
-            "minute": "23",
-            "hour": "00",
-            "day_of_week": "2",
-            "day_of_month": "23",
-            "month_of_year": "12",
-            "timezone": "UTC",
-        }
-        scheduled_task: Dict = {
-            "name": "task_1",
-            "task": "echo",
-            "schedule": schedule,
-        }
+
         new_schedule: Dict = {
             "minute": "24",
             "hour": "01",
@@ -100,11 +73,10 @@ def test_update_periodic_task():
             "task": "echo2",
             "schedule": new_schedule,
         }
-        schedule = CrontabSchedule(**schedule)
         new_schedule = CrontabSchedule(**new_schedule)
 
         periodic_task = PeriodicTask(
-            crontab=schedule,
+            crontab=CrontabSchedule(**scheduled_task["schedule"]),
             name=scheduled_task["name"],
             task=scheduled_task["task"],
         )
@@ -118,12 +90,14 @@ def test_update_periodic_task():
         periodic_task_id = 1
 
         updated_db_task = update_period_task(
-            mock_session, ScheduledTask.parse_obj(new_scheduled_task), periodic_task_id)
+            mock_session, ScheduledTask.parse_obj(new_scheduled_task), periodic_task_id
+        )
 
         assert mock_session.query(PeriodicTask).get.call_count == 1
         assert updated_task.name == updated_db_task.name
 
-        def test_delete_task(scheduled_task):
+
+def test_delete_task(scheduled_task):
     with patch("sqlalchemy.orm.Session") as mock_session:
         # Set up the mock_session
         periodic_task_id = 1
