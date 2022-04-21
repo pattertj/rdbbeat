f38e0c0fecfff17dc897012cc48020d8e9b3423b
Johanna Engelhard <johanna.engelhard@hpe.com>
2022-04-21 10:47:18 +0200

    version bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="celery_sqlalchemy_scheduler",
     python_requires=">=3.8",
     author="Aruba UXI",
-    version="0.1.0",
+    version="0.2.0",
     description="A Scheduler Based SQLalchemy For Celery",
     classifiers=[
         "Intended Audience :: Developers",
