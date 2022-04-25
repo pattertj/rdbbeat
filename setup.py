45faf474bc6f2d53a8351a4d0b3f6939f6cd7cb5
njabulo ndlovu <titannjabulo@gmail.com>
2022-04-25 11:10:07 +0200

    include type file appease mypy

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     name="uxi-celery-scheduler",
     python_requires=">=3.8",
     author="Aruba UXI",
-    version="1.3.1",
+    version="1.3.2",
     description="A SQLalchemy-based Scheduler For Celery Beat",
     classifiers=[
         "Intended Audience :: Developers",
@@ -19,7 +19,8 @@ setup(
         "Programming Language :: Python :: 3",
     ],
     url="https://github.com/aruba-uxi/celery-sqlalchemy-scheduler",
-    packages=find_packages(exclude=["contrib", "docs", "tests"]),
+    packages=find_packages(exclude=["tests"]),
+    package_data={"uxi_celery_scheduler": ["py.typed"]},
     include_package_data=True,
     zip_safe=False,
     install_requires=["celery~=5.2", "sqlalchemy~=1.4", "alembic", "pydantic"],
